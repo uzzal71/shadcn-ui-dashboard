5e8ed44765a68e10528d4165cfef8d27c3af50a7
uzzal71 <uzzalroy.acm@gmail.com>
2024-07-25 17:35:37 +0600

    Add the landing page text and buttons

---
+++ app/(logged-out)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function LadingPage() {
+    return (
+        <div>
+            <div>Support me</div>
+            <Button>Log in</Button>
+            <Button>Sign up</Button>
+        </div>
+    )
+}
