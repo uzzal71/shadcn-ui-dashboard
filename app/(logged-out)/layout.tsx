d9ac632195693faaa5e3bdc70cc108802e4dd37a
uzzal71 <uzzalroy.acm@gmail.com>
2024-07-27 16:44:45 +0600

    Build the light and dark model toggle

--- app/(logged-out)/layout.tsx
+++ app/(logged-out)/layout.tsx
@@ -1,11 +1,16 @@
-type Props = {
-    children?: React.ReactNode
-}
+import { LightDarkToggle } from "@/components/ui/light-dark-toggle";
 
-export default function LoggedOutlayout({ children }: Props) {
-    return (
-        <div className="flex flex-col gap-6 min-h-screen items-center justify-center p-24">
-            {children}
-        </div>
-    )
+type Props = {
+  children?: React.ReactNode;
+};
+
+export default function LoggedOutLayout({ children }: Props) {
+  return (
+    <>
+      <div className="flex flex-col gap-4 min-h-screen items-center justify-center p-24">
+        {children}
+      </div>
+      <LightDarkToggle className="fixed top-[calc(50%-12px)] right-2" />
+    </>
+  );
 }
