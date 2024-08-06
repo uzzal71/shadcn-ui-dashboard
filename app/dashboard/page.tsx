391197b67282cbec1eb71a65a5edca3f1b8ea4ca
uzzal71 <uzzalroy.acm@gmail.com>
2024-08-06 16:11:13 +0600

    Build the my dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,20 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import EmployeeStats from "./components/employees/employees-stats";
+import TeamsStats from "./components/teams/teams-stats";
+
 export default function DashboardPage() {
     return (
-        <div>
-            <h6>Dashboard page</h6>
-        </div>
+        <Tabs defaultValue="employees">
+            <TabsList className="mb-4">
+                <TabsTrigger value="employees">Employee stats</TabsTrigger>
+                <TabsTrigger value="teams">Teams stats</TabsTrigger>
+            </TabsList>
+            <TabsContent value="employees">
+                <EmployeeStats />
+            </TabsContent>
+            <TabsContent value="teams">
+                <TeamsStats />
+            </TabsContent>
+        </Tabs>
     )
 }
